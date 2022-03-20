442002e1a6b31e17aecee15f3aa33a9a0f030dc6
kaijietti <guokaijie45@gmail.com>
2022-03-21 00:39:22 +0800

    create network if need

--- globals.py
+++ globals.py
@@ -1,4 +1,5 @@
 import docker
+import click
 from iptables import IPTables
 from config import *
 
@@ -8,6 +9,27 @@ def init():
     docker_client = docker.from_env()
     global iptables
     iptables = IPTables()
+
+    # create graft-net if need
+    try:
+        docker_client.networks.get(GRAFTD_NETWORK)
+    except docker.errors.NotFound:
+        ipam_pool = docker.types.IPAMPool(
+            subnet='192.168.0.0/16',
+            gateway='192.168.0.1'
+        )
+
+        ipam_config = docker.types.IPAMConfig(
+            pool_configs=[ipam_pool]
+        )
+
+        docker_client.networks.create(
+            GRAFTD_NETWORK,
+            driver="bridge",
+            ipam=ipam_config
+        )
+        click.echo(f"successfully created docker-network:{GRAFTD_NETWORK}")
+
     # start long lived graftd client
     global graftd_client
     try:
